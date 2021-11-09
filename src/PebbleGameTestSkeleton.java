31bcea29d7d4258919901cc6e73e3e9042b8d545
umennear <um216@exeter.ac.uk>
2021-11-09 23:25:58 +0000

    Initial commit

---
+++ src/PebbleGameTestSkeleton.java
@@ -0,0 +1,60 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PebbleGameTestSkeleton {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getIfWinner() {
+    }
+
+    @Test
+    public void setWinnerTrue() {
+    }
+
+    @Test
+    public void createBlackBags() {
+    }
+
+    @Test
+    public void randomNumGenerator() {
+    }
+
+    @Test
+    public void checkFileInput() {
+    }
+
+    @Test
+    public void checkIntInput() {
+    }
+
+    @Test
+    public void gameMain() {
+    }
+
+    @Test
+    public void readFile() {
+    }
+
+    @Test
+    public void getNextPebble() {
+    }
+
+    @Test
+    public void playerWon() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
