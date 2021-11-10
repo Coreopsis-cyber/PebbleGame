3f643fa1af4b4b70dbdb67cc196008052632ad8d
Katie2211 <rosehopk@gmail.com>
2021-11-10 12:53:15 +0000

    10/11/2021 11:17

--- src/PebbleGameTestSkeleton.java
+++ src/PebbleGameTestSkeleton.java
@@ -3,8 +3,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import java.util.*;
+import java.io.*;
+
+import java.io.IOException;
 
 public class PebbleGameTestSkeleton {
+
+    static PebbleGame game = new PebbleGame();
+    Bags bag1 = new Bags();
+    Bags bag2 = new Bags();
+    Bags bag3 = new Bags();
 
     @Before
     public void setUp() throws Exception {
@@ -13,46 +22,90 @@ public class PebbleGameTestSkeleton {
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
-    public void getIfWinner() {
+    public void GetIfWinner() {
+        game.winner = false;
+        assert(game.getIfWinner() == false);
+        
     }
 
     @Test
     public void setWinnerTrue() {
+        game.setWinnerTrue();
+        assert(game.getIfWinner() == true);
     }
 
     @Test
     public void createBlackBags() {
+        int numberOfPlayers = 2;
+        boolean error = false;
+        try{
+            game.createBlackBags(numberOfPlayers, bag1, bag2, bag3);
+        }
+        catch(IOException e){
+            error = true;
+        }
+        assert(error = false);
+        assert(bag1.getBagPebbles().size() == 22);
+        assert(bag2.getBagPebbles().size() == 22);
+        assert(bag3.getBagPebbles().size() == 22);
+
+        
     }
 
     @Test
     public void randomNumGenerator() {
+        int randomNumber = game.randomNumGenerator(1,25);
+        assert(randomNumber >= 1);
+        assert(randomNumber <=25);
     }
 
     @Test
     public void checkFileInput() {
+        Scanner scanCorrect = new Scanner("exampleFile1.csv");
+        game.checkFileInput(scanCorrect, 1);
+        Scanner scanIncorrect = new Scanner("player1.txt");
+        game.checkFileInput(scanIncorrect, 1);
+        
     }
 
     @Test
     public void checkIntInput() {
+        Scanner scanCorrect = new Scanner("5");
+        game.checkIntInput(scanCorrect);
+        Scanner scanIncorrect = new Scanner("Five");
+        game.checkIntInput(scanIncorrect);
     }
 
     @Test
     public void gameMain() {
+        try{
+            PebbleGame.Player[] players = game.gameMain();
+            assert(players.length == 2);
+        }
+        catch(IOException e){
+            
+        }
     }
 
     @Test
     public void readFile() {
+        try{
+            File file = new File("exampleFile1");
+            assert(game.readFile(file) == "");
+        }
+        catch(IOException e){
+
+        }
     }
 
     @Test
     public void getNextPebble() {
-    }
-
-    @Test
-    public void playerWon() {
+        String data = "1,2,3,4,5,6,7,8,9";
+        assert(data.contains(game.getNextPebble(data)));
     }
 
     @Test
